125e7ed3d5609a61dc1a4c591cb9121f8ca63c21
Miwa-Abidin <miko.abidinov@gmail.com>
2023-01-20 12:44:25 +0600

    add Item model to branch Mirsaid

--- website/items/views.py
+++ website/items/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Item
+from rest_framework import viewsets
+from .serializers import ItemSerializers
+
+class ItemViewSet(viewsets.ModelViewSet):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializers
+
+
+
