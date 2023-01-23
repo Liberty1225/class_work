3ec645d57a8f2b2309b4f67e717cb4c616e13490
Miwa-Abidin <miko.abidinov@gmail.com>
2023-01-23 10:42:41 +0600

    Item serializers update and add ItemDetail serializer

--- website/items/serializers.py
+++ website/items/serializers.py
@@ -1,6 +1,13 @@
 from .models import Item
 from rest_framework import serializers
 
+
+class ItemDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Item
+        exclude = ['count']
+
+
 class ItemSerializers(serializers.ModelSerializer):
     class Meta:
         model = Item
