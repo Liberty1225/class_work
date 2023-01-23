490727bf1c18ad92f6f2baebc552b3dd1f2126eb
liberty1225 <aitegin1225@gmail.com>
2023-01-23 10:42:32 +0600

    item serializer update

--- website/items/serializers.py
+++ website/items/serializers.py
@@ -1,7 +1,11 @@
 from .models import Item
 from rest_framework import serializers
 
+
 class ItemSerializers(serializers.ModelSerializer):
-    class Meta:
-        model = Item
-        fields = '__all__'
+    name = serializers.CharField(max_length=20)
+    count = serializers.IntegerField()
+
+    def create(self, validated_data):
+        item = Item.objects.create(**validated_data)
+        return item
